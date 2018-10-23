f758988897e3afe6ae506fbd5c5022fb2543b8a7
Aluno <you@example.com>
2018-10-23 11:21:23 -0300

    classe autor e teste

---
+++ matheusmartins_python2.py
@@ -0,0 +1,42 @@
+class autor:
+
+	def __init__(self, primeiro_nome, ultimo_nome, data_nasc, meio_nome="",):
+		self.primeiro_nome = primeiro_nome
+		self.meio_nome = meio_nome
+		self.ultimo_nome = ultimo_nome
+		self.data_nasc = data_nasc
+
+	def nome_como_citado(self):
+		print( self.ultimo_nome.upper() + " " + self.primeiro_nome[0].upper() + ".")
+
+	def __str__(self):
+		return "{primeiro_nome}".format(primeiro_nome=self.primeiro_nome)
+
+class livro:
+
+	def __init__(self, titulo, ano, autores):
+		self.titulo = titulo
+		self.ano = ano
+		self.autores = [] #lista de autores
+
+	@property	
+	def titulo_vazio(self):
+		if len(self.titulo)==0:
+			raise ValueError("O titulo nao pode ser vazio")
+
+class biblioteca:
+
+	def __init__(self, livros):
+		self.livros = [] #lista de livros
+
+	def livros_por_autor():
+		 #utilizará a lista de livros para retornar um dicionário onde cada chave será o nome de um autor e, cada valor, será a lista de livros deste autor.
+		 dic = {}
+		 for livro in self.livros:
+		 	for autor in livro.autores:
+		 		dic[autor.primeiro_nome]=livro.titulo
+		 return dic
+
+if __name__ == "__main__":
+	jose = autor("José", "Augusto", "12/03/1978")
+	jose.nome_como_citado()
